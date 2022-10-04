a754362db2942e0096d66f80f441e7e95d94db61
Angel Gabriel Pagoaga <angelpagoaga8@gmail.com>
2022-10-04 10:58:03 -0600

    Correción de daoBase.

--- src/dao/models/entities/User.ts
+++ src/dao/models/entities/User.ts
@@ -1,3 +1,5 @@
+//Declaracion de la entidad usuario por medio de Interface.
+//Recordar que interface me describe la estructura de un objeto.
 export interface IUser{
     _id?: unknown;
     nombreCompleto: string;
---
+++ src/dao/models/sqlite/AbstractDao.ts
@@ -0,0 +1,1 @@
+import { IDaoObject } from "@server/dao/daoBase";
